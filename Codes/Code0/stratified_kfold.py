481aea1e7260c0b2c8ecc86695e600a938c52d4d
jamesliao2016 <453143942@qq.com>
2018-08-25 13:36:31 +0800

    eda 2

---
+++ Codes/Code0/stratified_kfold.py
@@ -0,0 +1,27 @@
+import numpy as np
+from sklearn.model_selection import KFold,StratifiedKFold
+
+X = np.array([
+    [1, 2, 3, 4],
+    [11, 12, 13, 14],
+    [21, 22, 23, 24],
+    [31, 32, 33, 34],
+    [41, 42, 43, 44],
+    [51, 52, 53, 54],
+    [61, 62, 63, 64],
+    [71, 72, 73, 74]
+])
+
+y = np.array([0, 0, 0, 0, 1, 1, 1, 1])
+# n_folds这个参数没有，引入的包不同，
+floder = KFold(n_splits=4, random_state=0, shuffle=False)
+sfolder = StratifiedKFold(n_splits=4, random_state=0, shuffle=False)
+
+for train, test in sfolder.split(X, y):
+    print('Train: %s | test: %s' % (train, test))
+    print(" ")
+print('#######################')
+
+for train, test in floder.split(X, y):
+    print('Train: %s | test: %s' % (train, test))
+    print(" ")
