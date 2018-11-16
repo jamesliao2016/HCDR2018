6d39d90a7cd347051e8ed2aef26296e805d143e4
jamesliao2016 <453143942@qq.com>
2018-11-16 10:40:34 +0800

    1116

--- LT2018/Sort/quick_sort_1_1115.py
+++ LT2018/Sort/quick_sort_1_1115.py
@@ -4,38 +4,60 @@
 '''
 @author: willard
 '''
+#
+# def quick_sort_standord(array,low,high):
+#     ''' realize from book "data struct" of author 严蔚敏
+#     '''
+#     if low < high:
+#         key_index = partion(array,low,high)
+#         quick_sort_standord(array,low,key_index)
+#         quick_sort_standord(array,key_index+1,high)
+#
+# def partion(array,low,high):
+#     key = array[low]
+#     while low < high:
+#         while low < high and array[high] >= key:
+#             high -= 1
+#         # print(high)
+#         # print(key)
+#         if low < high:
+#             array[low] = array[high]
+#
+#         while low < high and array[low] < key:
+#             low += 1
+#         # print(low)
+#         if low < high:
+#             array[high] = array[low]
+#         print(low,high)
+#
+#     array[low] = key
+#     return low
 
-def quick_sort_standord(array,low,high):
-    ''' realize from book "data struct" of author 严蔚敏
-    '''
-    if low < high:
-        key_index = partion(array,low,high)
-        quick_sort_standord(array,low,key_index)
-        quick_sort_standord(array,key_index+1,high)
 
-def partion(array,low,high):
-    key = array[low]
+def quick_sort_standord(nums,low,high):
+    if low<high:
+        keyind=partition(nums,low,high)
+        quick_sort_standord(nums,low,keyind)
+        quick_sort_standord(nums,keyind+1,high)
+
+def partition(nums,low,high):
+    val_key = nums[low]
     while low < high:
-        while low < high and array[high] >= key:
+        while low < high and nums[high] >= val_key:
             high -= 1
-        # print(high)
-        # print(key)
         if low < high:
-            array[low] = array[high]
-
-        while low < high and array[low] < key:
+            nums[low] = nums[high]
+        while low < high and nums[low] < val_key:
             low += 1
-        # print(low)
         if low < high:
-            array[high] = array[low]
-        print(low,high)
-
-    array[low] = key
+            nums[high] = nums[low]
+    nums[low] = val_key
     return low
 
 if __name__ == '__main__':
     # array2 = [9,3,2,1,4,6,7,0,5]
-    array2 = [3,2,1,5,6,4]
+    # array2 = [3,2,1,5,6,4]
+    array2 = [4,6,3,2,1,5]
 
     print (array2)
     quick_sort_standord(array2,0,len(array2)-1)
