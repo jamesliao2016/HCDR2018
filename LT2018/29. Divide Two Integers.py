f110427777963d31e0c6fab8374165f0f036ecc6
jamesliao2016 <453143942@qq.com>
2018-11-18 11:12:18 +0800

    1118

--- LT2018/29. Divide Two Integers.py
+++ LT2018/29. Divide Two Integers.py
@@ -5,29 +5,17 @@ class Solution:
         :type divisor: int
         :rtype: int
         """
-        tmp_diff_pos = dividend - divisor
-        tmp_diff_neg = dividend + divisor
-        val_sign = 1
-        if (abs(dividend)>dividend and abs(divisor)==divisor) or (abs(dividend)==dividend and abs(divisor)>divisor):
-            val_sign = -1
-            dividend = abs(dividend)
-            divisor = abs(divisor)
-        # t = 0
-        # while dividend > divisor:
-        #     dividend -= divisor
-        #     t += 1
-        # return t * val_sign
-
+        val_postv = 1 if (dividend > 0) == (divisor > 0) else -1
+        dividend,divisor = abs(dividend),abs(divisor)
         res = 0
-        while divisor <= dividend:
-            tmp,i = divisor,1
-            while tmp <= dividend:
-                res += i
-                dividend -= tmp
-                tmp <<= 1
-                i <<= 1
-        return val_sign * res
-
+        while dividend > divisor:
+            i,tmp = divisor,1
+            while dividend > i:
+                res += tmp
+                dividend -= i
+                i<<=1
+                tmp<<=1
+        return res if val_postv == 1 else -res
 
 
 if __name__ == '__main__':
