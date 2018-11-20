7ab7d34fd9942bd844e3e39fa663026f55317f55
jamesliao2016 <453143942@qq.com>
2018-11-20 10:19:47 +0800

    1120

--- LT2018/DynamicProgramming/5. Longest Palindromic Substring.py
+++ LT2018/DynamicProgramming/5. Longest Palindromic Substring.py
@@ -4,7 +4,41 @@ class Solution:
         :type s: str
         :rtype: str
         """
-        def toolfun(i,j):
+        def ret_tmp(s,l,r):
+            res = []
+            while l>=0 and r<len(s) and s[l] == s[r]:
+                res = s[l:r + 1]
+                l-=1;r+=1
+            return res
+        val_len = 0
+        res = []
+        tmp_res = []
+        for i in range(len(s)):
+            str_odd = ret_tmp(s,i,i)
+            str_even = ret_tmp(s,i,i+1)
+            if len(str_even)>len(str_odd):
+                tmp_res = str_even
+            else:
+                tmp_res = str_odd
+            if len(tmp_res) > len(res):
+                res = tmp_res
+        return res
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    data_input = 'babad'
+    # data_input = 'cbbd'
+    data_output = Solution()
+    print(data_output.longestPalindrome(data_input))
+
+    '''
+    # 20 nov, 2018
+            def toolfun(i,j):
             tmp_res = ''
             while i>=0 and j<len(s) and s[i]==s[j]:
 
@@ -29,9 +63,4 @@ class Solution:
                 res = tmp
         return res
 
-
-if __name__ == '__main__':
-    # data_input = 'babad'
-    data_input = 'bb'
-    data_output = Solution()
-    print(data_output.longestPalindrome(data_input))
+    '''
