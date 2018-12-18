96a691b504689d114afd9cee741067898ad3baba
jamesliao2016 <453143942@qq.com>
2018-12-18 10:35:18 +0800

    1218

---
+++ LT2018/Heap/239. Sliding Window Maximum.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python2
+# coding:utf-8
+
+class Solution(object):
+    def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        res = []
+        if not nums:
+            for i in range(len(nums) - k +1):
+                tmp = nums[i:i+k]
+                res.append(max(tmp))
+        return res
+
+if __name__ == '__main__':
+    nums = [1, 3, -1, -3, 5, 3, 6, 7]; k = 3
+    print(Solution().maxSlidingWindow(nums,k))
+
+    '''
+    Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position. Return the max sliding window.
+
+Example:
+
+Input: nums = [1,3,-1,-3,5,3,6,7], and k = 3
+Output: [3,3,5,5,6,7] 
+Explanation: 
+
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+Note: 
+You may assume k is always valid, 1 ≤ k ≤ input array's size for non-empty array.
+
+Follow up:
+Could you solve it in linear time?
+    '''
