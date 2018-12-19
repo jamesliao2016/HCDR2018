319268f559b0e673da128a195a7572458040180d
jamesliao2016 <453143942@qq.com>
2018-12-19 10:04:25 +0800

    1219

--- LT2018/ListNode/21. Merge Two Sorted Lists.py
+++ LT2018/ListNode/21. Merge Two Sorted Lists.py
@@ -13,6 +13,43 @@ class Solution:
         :type l2: ListNode
         :rtype: ListNode
         """
+        res = tmp = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                tmp.next = ListNode(l1.val)
+                tmp = tmp.next
+                l1 = l1.next
+            else:
+                tmp.next = ListNode(l2.val)
+                tmp = tmp.next
+                l2 = l2.next
+        tmp.next = l1 or l2
+        return res.next
+
+
+if __name__ == '__main__':
+
+    l1=ListNode(2)
+    l1.next = ListNode(4)
+    l1.next.next = ListNode(3)
+
+    l2=ListNode(5)
+    l2.next = ListNode(6)
+    l2.next.next = ListNode(4)
+
+    print(Solution.mergeTwoLists(l1,l2))
+
+'''
+
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+
+dec 19
+
         res = tmp_root = ListNode(0)
         while l1 or l2:
             if l1 and l2:
@@ -33,15 +70,4 @@ class Solution:
                 tmp_root = tmp_root.next
                 l2 = l2.next
         return res.next
-
-if __name__ == '__main__':
-
-    l1=ListNode(2)
-    l1.next = ListNode(4)
-    l1.next.next = ListNode(3)
-
-    l2=ListNode(5)
-    l2.next = ListNode(6)
-    l2.next.next = ListNode(4)
-
-    print(Solution.mergeTwoLists(l1,l2))
+'''
