ea1156028afc786b9a2a4c4bfd06d03e48067672
jamesliao2016 <453143942@qq.com>
2018-12-21 09:48:56 +0800

    1221

--- LT2018/HashTable/3. Longest Substring Without Repeating Characters.py
+++ LT2018/HashTable/3. Longest Substring Without Repeating Characters.py
@@ -4,41 +4,31 @@ class Solution:
         :type s: str
         :rtype: int
         """
+        dict_s = {}
         res = 0
-        starter = 0
-        dict_n = {}
+        l = 0
         for i in range(len(s)):
-            if s[i] in dict_n:
-                starter = max(starter,dict_n[s[i]]+1)
-                res = max(res,i - starter + 1)
+            if s[i] not in dict_s:
+                res = max(res,i - l + 1)
+                # l = i+1
             else:
-                res = max(res, i - starter + 1)
-            dict_n[s[i]] = i
+                # if l>0:
+                l = max(l,dict_s[s[i]]+1)
+                res = max(res, i - l + 1)
+            dict_s[s[i]] = i
         return res
+
 
 if __name__ == '__main__':
     # data_input = "bbbbb"
     # data_input = "pwwkew"
     # data_input = "abcabcbb"
-    data_input = "anviaj"
+    # data_input = "anviaj"
+    data_input = "abba"
     data_output = Solution()
     print(data_output.lengthOfLongestSubstring(data_input))
 
 '''
-6 dec
-        if s == '':
-            return 0
-        l1,l2 = 0,1
-        res=0
-        while l2<=len(s)-1:
-            if len(set(s[l1:l2+1])) == l2+1-l1:
-                l2+=1
-                res = max(res, l2 - l1)
-            else:
-                l1 +=1
-                # l1 = l2
-                # l2 = l1+1
-        return res
 
 Given a string, find the length of the longest substring without repeating characters.
 
@@ -58,4 +48,34 @@ Input: "pwwkew"
 Output: 3
 Explanation: The answer is "wke", with the length of 3. 
              Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+             
+             # dec 21
+             
+                     res = 0
+        starter = 0
+        dict_n = {}
+        for i in range(len(s)):
+            if s[i] in dict_n:
+                starter = max(starter,dict_n[s[i]]+1)
+                res = max(res,i - starter + 1)
+            else:
+                res = max(res, i - starter + 1)
+            dict_n[s[i]] = i
+        return res
+        
+        6 dec
+        if s == '':
+            return 0
+        l1,l2 = 0,1
+        res=0
+        while l2<=len(s)-1:
+            if len(set(s[l1:l2+1])) == l2+1-l1:
+                l2+=1
+                res = max(res, l2 - l1)
+            else:
+                l1 +=1
+                # l1 = l2
+                # l2 = l1+1
+        return res
+
 '''
--- LT2018/ListNode/2. Add Two Numbers.py
+++ LT2018/ListNode/2. Add Two Numbers.py
@@ -11,19 +11,19 @@ class Solution:
         :type l2: ListNode
         :rtype: ListNode
         """
-        res = tmp_root = ListNode(0)
-        tmp_ten = 0
-        while l1 or l2 or tmp_ten:
-            tmp_1,tmp_2 = 0,0
+        res = tmp = ListNode(0)
+        val = 0
+        while l1 or l2 or val:
+            v1,v2= 0,0
             if l1:
-                tmp_1 = l1.val
-                l1 = l1.next
+                v1 = l1.val
+                l1= l1.next
             if l2:
-                tmp_2 = l2.val
+                v2 = l2.val
                 l2 = l2.next
-            tmp_ten,tmp_cur = divmod(tmp_ten+tmp_1+tmp_2,10)
-            tmp_root.next = ListNode(tmp_cur)
-            tmp_root = tmp_root.next
+            val,v0 = divmod(v1+v2+val,10)
+            tmp.next = ListNode(v0)
+            tmp = tmp.next
         return res.next
 
 
@@ -55,6 +55,35 @@ if __name__ == '__main__':
     #     print(tmp.val)
     #     tmp=tmp.next
 
+
+'''
+
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example:
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+
+DEC 21
+        res = tmp_root = ListNode(0)
+        tmp_ten = 0
+        while l1 or l2 or tmp_ten:
+            tmp_1,tmp_2 = 0,0
+            if l1:
+                tmp_1 = l1.val
+                l1 = l1.next
+            if l2:
+                tmp_2 = l2.val
+                l2 = l2.next
+            tmp_ten,tmp_cur = divmod(tmp_ten+tmp_1+tmp_2,10)
+            tmp_root.next = ListNode(tmp_cur)
+            tmp_root = tmp_root.next
+        return res.next
+
     # res = n = ListNode(0)
     # v_carry = 0
     # while l1 or l2:
@@ -69,3 +98,5 @@ if __name__ == '__main__':
     #     n.next = ListNode(tmp)
     #     n = n.next
     # return res
+
+'''
