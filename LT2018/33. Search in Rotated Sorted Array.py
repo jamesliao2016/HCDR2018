85759f9e513185707a1cbde58a2952b90c2d6fcb
jamesliao2016 <453143942@qq.com>
2019-01-16 14:00:42 +0800

    JAN 16

--- LT2018/33. Search in Rotated Sorted Array.py
+++ LT2018/33. Search in Rotated Sorted Array.py
@@ -8,6 +8,73 @@ class Solution:
         if not nums:
             return -1
         l,r = 0,len(nums) - 1
+        while l<=r:
+            mid = int((l+r)/2)
+            if nums[mid] == target:
+                return mid
+            if nums[mid]>=nums[l]:
+                if nums[mid]>=target>=nums[l]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid]<=target<=nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return -1
+
+if __name__ == '__main__':
+    y = Solution()
+    nums = [4, 5, 6, 7, 0, 1, 2];target = 0
+    # nums = [4,5,6,7,0,1,2]; target = 6
+    print(y.search(nums,target))
+'''
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+
+You are given a target value to search. If found in the array return its index, 
+otherwise return -1.
+
+You may assume no duplicate exists in the array.
+
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+
+# 16 jan, 2019
+        if not nums:
+            return -1
+        l,r = 0,len(nums) - 1
+        while l<=r:
+            mid = int((l+r)/2)
+            if nums[mid] == target:
+                return mid
+            if nums[mid]>=nums[l]:
+                if nums[mid]>= target >=nums[l]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid]<=target<=nums[r]:
+                    l = mid +1
+                else:
+                    r = mid - 1
+        return -1
+
+
+# 15 JAN, 2019
+        if not nums:
+            return -1
+        l,r = 0,len(nums) - 1
 
         while l<=r:
             mid = int((l + r) /2)
@@ -50,11 +117,5 @@ class Solution:
         #
         # return -1
 
-
-if __name__ == '__main__':
-    y = Solution()
-    # dividend = 10; divisor = 3
-    nums = [4,5,6,7,0,1,2]; target = 6
-    print(y.search(nums,target))
-
+'''
 
--- LT2018/BinarySearch/50. Pow(x, n).py
+++ LT2018/BinarySearch/50. Pow(x, n).py
@@ -8,6 +8,46 @@ class Solution:
         :type n: int
         :rtype: float
         """
+        if n==0:
+            return 1
+        if n<0:
+            return self.myPow(1/x,-n)
+        if n>0:
+            if n%2==0:
+                return self.myPow(x*x,int(n/2))
+            else:
+                return self.myPow(x*x,int(n/2))*x
+
+if __name__ == '__main__':
+    # x1=2.1
+    # x2=-5
+    # x1=1.00001; x2=123456
+    x1 = 2;    x2 = 10
+    output_y=Solution()
+    print(output_y.myPow(x1,x2))
+
+'''
+Implement pow(x, n), which calculates x raised to the power n (xn).
+
+Example 1:
+
+Input: 2.00000, 10
+Output: 1024.00000
+Example 2:
+
+Input: 2.10000, 3
+Output: 9.26100
+Example 3:
+
+Input: 2.00000, -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+Note:
+
+-100.0 < x < 100.0
+n is a 32-bit signed integer, within the range [−231, 231 − 1]
+
+16 jan, 2019
         print(n)
         if not n:
             return 1
@@ -17,8 +57,4 @@ class Solution:
             return x*self.myPow(x,n-1)
         return self.myPow(x*x,n/2)
 
-if __name__ == '__main__':
-    x1=2.1
-    x2=-5
-    output_y=Solution()
-    print(output_y.myPow(x1,x2))
+'''
