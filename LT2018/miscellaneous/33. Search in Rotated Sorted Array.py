cc6e8886dbe4d72d33ad72c32a148dd417de76e8
jamesliao2016 <453143942@qq.com>
2019-05-17 09:55:46 +0800

    may 17

--- LT2018/miscellaneous/33. Search in Rotated Sorted Array.py
+++ LT2018/miscellaneous/33. Search in Rotated Sorted Array.py
@@ -7,13 +7,13 @@ class Solution:
         """
         if not nums:
             return -1
-        l,r = 0,len(nums) - 1
+        l,r = 0,len(nums)-1
         while l<=r:
             mid = int((l+r)/2)
             if nums[mid] == target:
                 return mid
-            if nums[mid]>=nums[l]:
-                if nums[mid]>=target>=nums[l]:
+            if nums[l] <= nums[mid]:
+                if nums[l]<=target<=nums[mid]:
                     r = mid - 1
                 else:
                     l = mid + 1
@@ -49,6 +49,27 @@ Example 2:
 
 Input: nums = [4,5,6,7,0,1,2], target = 3
 Output: -1
+
+# 17 may, 2019
+        if not nums:
+            return -1
+        l,r = 0,len(nums) - 1
+        while l<=r:
+            mid = int((l+r)/2)
+            if nums[mid] == target:
+                return mid
+            if nums[mid]>=nums[l]:
+                if nums[mid]>=target>=nums[l]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid]<=target<=nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return -1
+
 
 # 16 jan, 2019
         if not nums:
